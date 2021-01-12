c52089fba30d255d412d8bcfa5d47bb2c3fff76b
Selena Wells <selena.elise.wells@gmail.com>
2021-01-12 14:30:04 -0500

    changes to try and fix hosting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 //to make this more modern use JS import
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
