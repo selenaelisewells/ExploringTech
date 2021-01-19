92a061474ed9dd04ea04e4bef68eee644e5e933e
Selena Wells <selena.elise.wells@gmail.com>
2021-01-19 12:50:27 -0500

    app.js case statements added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const stream = require('fs');
 
 //to make this more modern use JS import
 
@@ -8,6 +9,32 @@ const port = process.env.PORT || 3000; //localhost: 3000 - says if this fails us
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+
+    let url = req.url; //localhost:3000/contact
+
+    switch (url) {
+        case "/contact":
+            stream.readFile('contact.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found');
+                } else {
+                    res.write(data);
+                }
+            }))
+            break;
+        case "/portfolio":
+            stream.readFile('portfolio.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found');
+                } else {
+                    res.write(data);
+                }
+            }))
+            break;
+    }
+
     res.end('Hello Darkness');
 });
 
