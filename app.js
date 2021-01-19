e639a8cf016ed1381ce22633404a1fa1412e8873
Selena Wells <59712809+selenaelisewells@users.noreply.github.com>
2021-01-19 12:36:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 //to make this more modern use JS import
 
-const hostname = '127.0.0.1';
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -11,6 +11,6 @@ const server = http.createServer((req, res) => {
     res.end('Hello Darkness');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port,() => {
+    console.log(`Server running at ${port}/`);
 });
